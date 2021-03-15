c5baa344f8547fafb17b7f78ef5f262c28ec84e8
Jay Allard <john_allard@trimble.com>
2021-03-15 17:24:19 -0400

    checkpoint

---
+++ Allard.Configinator.Core.Tests/Tools.cs
@@ -0,0 +1,16 @@
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Allard.Configinator.Infrastructure.MongoDb;
+using Xunit;
+
+namespace Allard.Configinator.Core.Tests
+{
+    public class Tools
+    {
+        [Fact]
+        public async Task SetupDatabase()
+        {
+            await new OrganizationRepositoryMongo().DevelopmentSetup();
+        }
+    }
+}
